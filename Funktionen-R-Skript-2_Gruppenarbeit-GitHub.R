bd4aa21685fd732678cf6a137a77107b39f00f9c
carolinebaer <94549515+carolinebaer@users.noreply.github.com>
2022-01-03 11:10:20 +0100

    Aufgabenstellung fürs 2.Skript erweitert
    ich hab mal die Erklärung aus der Email von eben noch ergänzt

--- Funktionen-R-Skript-2_Gruppenarbeit-GitHub.R
+++ Funktionen-R-Skript-2_Gruppenarbeit-GitHub.R
@@ -6,4 +6,9 @@
 #sondern die nur in Funktionen-Skript 1 Anwendung finden (interne Funktionen).
 #Funktionen-R-Skript 2 muss mindestens eine Funktion enthalten.
 
+#(EMail) Manche Berechnungen benoetigt ihr moeglicherweise fuer mehrere Funktionen.  
+#Da ihr diese Berechnung nicht in jeder Funktion neu implementieren moechtet, 
+#koennt ihr dafuer eine so genannte Hilfsfunktion schreiben, die ihr dann 
+#innerhalb der eigentlichen Funktionen immer wieder verwenden könnt. Diese 
+#Hilfsfunktionen sollen in ein extra R-Script geschrieben werden.
 
