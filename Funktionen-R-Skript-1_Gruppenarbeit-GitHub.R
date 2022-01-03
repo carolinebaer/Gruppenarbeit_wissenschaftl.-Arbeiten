a14d8a8848393e2d1f6edddd772548b74be0f869
carolinebaer <94549515+carolinebaer@users.noreply.github.com>
2022-01-03 10:53:22 +0100

    Erstellung der R-Skripte
    nur die Struktur, noch ohne Inhalt

---
+++ Funktionen-R-Skript-1_Gruppenarbeit-GitHub.R
@@ -0,0 +1,52 @@
+### Gruppenarbeit - wissenschaftliches Arbeiten ###
+## Funktionen-R-Skript-1 ##
+
+#_______________________________________________________________________________
+#(a) Eine Funktion, die verschiedene geeignete deskriptive Statistiken 
+#fuer metrische Variablen berechnet und ausgibt
+
+
+
+
+#_______________________________________________________________________________
+#(b) Eine Funktion, die verschiedene geeignete deskriptive Statistiken 
+#fuer kategoriale Variablen berechnet und ausgibt
+
+
+
+
+#_______________________________________________________________________________
+#(c) Eine Funktion, die geeignete deskriptive bivariate Statistiken fuer 
+#den Zusammenhang zwischen zwei kategorialen Variablen berechnet ausgibt
+
+
+
+
+#_______________________________________________________________________________
+#(d) Eine Funktion, die geeignete deskriptive bivariate Statistiken fuer 
+#den Zusammengang zwischen einer metrischen und einer dichotomen Variablen 
+#berechnet und ausgibt
+
+
+
+
+#_______________________________________________________________________________
+#(e) Eine Funktion, die eine mindestens ordinal skalierte Variable 
+#quantilbasiert kategorisiert (z.B. in "niedrig", "mittel", "hoch")
+
+
+
+
+#_______________________________________________________________________________
+#(f)Eine Funktion, die eine geeignete Visualisierung von drei oder vier 
+#kategorialen Variablen erstellt
+
+
+
+
+#_______________________________________________________________________________
+#Freiwillig: weitere zur Deskription und Visualisierung geeignete 
+#Funktionen
+
+
+
---
+++ Funktionen-R-Skript-2_Gruppenarbeit-GitHub.R
@@ -0,0 +1,9 @@
+### Gruppenarbeit - wissenschaftliches Arbeiten ###
+## Funktionen-R-Skript-2 ##
+
+#(Aufgabenstellung) Funkionen-R-Skript 2 soll Helfer-Funktionen enthalten, die 
+#nicht selbst zur Deskription und Visualisierung der Daten verwendet werden, 
+#sondern die nur in Funktionen-Skript 1 Anwendung finden (interne Funktionen).
+#Funktionen-R-Skript 2 muss mindestens eine Funktion enthalten.
+
+
