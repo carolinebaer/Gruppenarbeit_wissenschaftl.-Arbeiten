602a6223dfd072d356e0d84ff8abd38655ccc41d
carolinebaer <94549515+carolinebaer@users.noreply.github.com>
2022-02-18 19:38:40 +0100

    Umlaute und optische Änderung der Spaltenbreite

--- Funktionen-R-Skript-1_Gruppenarbeit-GitHub.R
+++ Funktionen-R-Skript-1_Gruppenarbeit-GitHub.R
@@ -101,7 +101,8 @@ zsmhang <- function(GruppeEins, GruppeZwei, names = c("GruppeEins", "GruppeZwei"
 #    dev.off()
 #}
 
-MetrischDichotom<- function(VarMetrisch, VarDichotom) #eingabe mit Tabelle$Spaltenname 
+MetrischDichotom<- function(VarMetrisch, VarDichotom) #eingabe mit 
+                                                        #Tabelle$Spaltenname 
 {
   #anzeigen:
   boxplot(VarMetrisch~VarDichotom, daten, 
@@ -148,25 +149,38 @@ MetrischDichotom<- function(VarMetrisch, VarDichotom) #eingabe mit Tabelle$Spalt
 # }
 
 umkodieren <- function(Variable){
-  if(max(range(Variable)) %% 3 == 0){  # wenn die Spannweite der Auspraegungen  ohne Rest durch drei teilbar ist
+  if(max(range(Variable)) %% 3 == 0){  # wenn die Spannweite der Auspraegungen  
+                                          #ohne Rest durch drei teilbar ist
     Varibale[Variable == c(min(Variable): max(range(Variable)/3))] <- "niedrig"
-    Varibale[Variable == c(max(range(Variable))/3 +1 : 2*(max(range(Variable)/3)))] <- "mittel"
-    Varibale[Variable == c(2*(max(range(Variable)/3)) +1 : max(Variable))] <- "hoch"
+    Varibale[Variable == 
+            c(max(range(Variable))/3 +1 : 2*(max(range(Variable)/3)))] <- "mittel"
+    Varibale[Variable == 
+               c(2*(max(range(Variable)/3)) +1 : max(Variable))] <- "hoch"
   } else 
-    if(max(range(Variable)) %% 3 == 1){ # wenn der Rest der Division durch drei 1 ist     
+    if(max(range(Variable)) %% 3 == 1){ 
+      #wenn der Rest der Division durch drei 1 ist     
       Varibale[Variable == c(min(Variable): max(range(Variable)/3))] <- "niedrig"
-      Varibale[Variable == c(max(range(Variable))/3 +1 : 2*(max(range(Variable))/3))] <- "mittel"
-      # ich wei� nicht warum aber wenn es h�ndisch ausprobiere kommen bei mir als untere und obere Grenze desvektoren andere Zahlen raus, als wenn ich den Doppelpunkt dazwischen setzte
-      # Zahlenbeispiel max(Range(Variable))==7 -> max(range(Variable))/3 +1= 4/3, max(range(Variable))/3 +1 = 4+2/3 
-      # c(max(range(Variable))/3 +1 : 2*(max(range(Variable))/3)) der Vektor ist jedoch dann 4.666667 7.000000
-      #oder habe ich gerade hier einen Gro�en Denkfehler, freue mich auf eure Anregungen
+      Varibale[Variable == 
+             c(max(range(Variable))/3 +1 : 2*(max(range(Variable))/3))] <- "mittel"
+      # ich weiss nicht warum aber wenn es haendisch ausprobiere kommen bei mir 
+      #  als untere und obere Grenze desvektoren andere Zahlen raus, als wenn ich 
+      #  den Doppelpunkt dazwischen setzte
+      # Zahlenbeispiel max(Range(Variable))==7 -> max(range(Variable))/3 +1= 4/3, 
+      #  max(range(Variable))/3 +1 = 4+2/3 
+      # c(max(range(Variable))/3 +1 : 2*(max(range(Variable))/3)) der Vektor ist 
+      #  jedoch dann 4.666667 7.000000
+      #oder habe ich gerade hier einen Grossen Denkfehler, freue mich auf eure 
+      # Anregungen
       
-      Varibale[Variable == c(2*(max(range(Variable))/3) +1 : max(Variable)-1)] <- "hoch"
+      Varibale[Variable == 
+                 c(2*(max(range(Variable))/3) +1 : max(Variable)-1)] <- "hoch"
       Varibale[Variable == max(Variable)] <- "sehr hoch"
     } else {                   # wenn der Rest der Division durch drei 2 ist  
       Varibale[Variable == c(min(Variable): range(Variable)/3)] <- "niedrig"
-      Varibale[Variable == c(max(range(Variable))/3 +1 : 2*(max(range(Variable))/3))] <- "mittel"
-      Varibale[Variable == c(2*(max(range(Variable))/3) +1 : max(Variable)-2)] <- "hoch"
+      Varibale[Variable == 
+             c(max(range(Variable))/3 +1 : 2*(max(range(Variable))/3))] <- "mittel"
+      Varibale[Variable == 
+                 c(2*(max(range(Variable))/3) +1 : max(Variable)-2)] <- "hoch"
       Varibale[Variable == c(max(Variable)-1 , max(Variable))] <- "sehr hoch"   
     }
   
