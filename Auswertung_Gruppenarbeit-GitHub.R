237a7cd267960350ee0d60d0debe1ece0223f69c
leafreyy <94891139+leafreyy@users.noreply.github.com>
2022-02-23 22:13:41 +0100

    Skript 4
    das R-Skript für die Auswertung

---
+++ Auswertung_Gruppenarbeit-GitHub.R
@@ -0,0 +1,16 @@
+### Gruppenarbeit - wissenschaftliches Arbeiten ###
+## Auswertung der Daten ##
+
+# Datensatz laden
+
+
+
+
+# Auswetung der Daten mit Hilfe der Funktionen aus Skript 1
+
+
+
+
+
+
+# Visualisierung der Daten
